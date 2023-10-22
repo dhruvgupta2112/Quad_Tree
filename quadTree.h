e7bfb46a134236aaab18653761f0204b6af283ab
dhruv <dhruvgupta.er@gmail.com>
2023-10-23 00:34:59 +0530

    points are updated to double

--- quadTree.h
+++ quadTree.h
@@ -3,21 +3,24 @@
 using namespace std;
 
 class Point{
-    int x, y;
-    Point(int x = 0, int y = 0): x(x), y(y){}
+    double x, y;
+    Point(double x = 0, double y = 0): x(x), y(y){}
 };
 
 class Rectangle{
     // w = widht 
     // h = height
-    int x, y, w, h;
+    double x, y, w, h;
+    Rectangle(double x, double y, double w, double h): x(x), y(y), w(w), h(h){}
+    bool contains(const Point &);
+    bool intersects(const Rectangle&);
 };
 
 class quadTree{
 private:
     Rectangle boundary;
     int capacity;
-    vector<Point> pt;
+    vector<Point> points;
     bool divided;
 public:
     void insert();
