1db2112b746422cf09879a9404cf00dc63757d96
dhruv <dhruvgupta.er@gmail.com>
2023-10-22 23:40:08 +0530

    error

--- quadTree.h
+++ quadTree.h
@@ -1,3 +1,20 @@
 #pragma once
 #include<bits/stdc++.h>
 using namespace std;
+
+class Point{
+    int x, y;
+};
+
+class Rectangle{
+    // w = widht 
+    // h = height
+    int x, y, w, h;
+};
+
+class QuadTree{
+    Rectangle boundary;
+    int capacity;
+    vector<Point> pt;
+};
+
