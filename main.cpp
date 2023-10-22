a4b0563b244951841c2838efab667c5cdd0a4184
dhruv <dhruvgupta.er@gmail.com>
2023-10-22 23:35:57 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+#include "quadTree.cpp"
+using namespace std;
+
+int main(){
+    
+}
---
+++ quadTree.cpp
@@ -0,0 +1,5 @@
+#include "quadTree.h"
+
+void print(){
+    std::cout<<"hello duniya!";
+}
