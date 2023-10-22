b0b75debce615dfe503194408c4611243cf2c587
dhruv <dhruvgupta.er@gmail.com>
2023-10-23 00:43:15 +0530

    update rectangle functions in quadTee.cpp

--- quadTree.cpp
+++ quadTree.cpp
@@ -1,9 +1,16 @@
 #include "quadTree.h"
 
-// bool Rectangle::contains(const Point & point){
-//     return (point.x >= x - w/2 && point.x< x + );
-// }
+bool Rectangle::contains(const Point & point){
+    return (point.x >= x - w/2 && point.x <= x + w/2 &&
+            point.y >= y - h/2 && point.y <= h/2);
+}
 
+bool Rectangle::intersects(const Rectangle & R2){
+    return !(R2.x - R2.w/2 > x + w/2 ||
+             R2.x + R2.w/2 < x - w/2 ||
+             R2.y - R2.h/2 > y + h/2 ||
+             R2.y + R2.h/2 < y - h/2);
+}
 void quadTree::insert(){
 
 }
--- quadTree.h
+++ quadTree.h
@@ -3,6 +3,7 @@
 using namespace std;
 
 class Point{
+public: 
     double x, y;
     Point(double x = 0, double y = 0): x(x), y(y){}
 };
